40e76240a353e9ee954486b98e55557520db29dc
Petr Morávek <moravp15@fit.cvut.cz>
2024-03-23 21:51:31 +0100

    comments

--- src/vhosts_command/vhosts_cmd.py
+++ src/vhosts_command/vhosts_cmd.py
@@ -2,10 +2,21 @@ from abc import ABC, abstractmethod
 
 
 class VhostsCmd(ABC):
+    """
+    Interface representing string commands used for querying on Linux based machines.
+    These queries are used for searching virtual hosts.
+    Each of the subclass of this interface should represent concrete web server.
+    """
     @abstractmethod
     def is_web_server_running(self) -> str:
+        """
+        Query used for checking if the web server is running
+        """
         pass
 
     @abstractmethod
     def get_all_vhosts(self) -> str:
+        """
+        Query used for getting all virtual hosts of the web server
+        """
         pass

f324cd8bcac61f2aae4f91cd3398a043bc309895
Petr Morávek <moravp15@fit.cvut.cz>
2024-03-31 19:53:19 +0200

    add options to chrome renderer

--- src/client_side_renderer/selenium_chrome_renderer.py
+++ src/client_side_renderer/selenium_chrome_renderer.py
@@ -2,8 +2,13 @@ from src.client_side_renderer.selenium_renderer import SeleniumRenderer
 import selenium
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
 
 
 class SeleniumChromeRenderer(SeleniumRenderer):
     def _get_driver(self) -> selenium.webdriver.remote.webdriver.WebDriver:
-        return webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--incognito")
+        return webdriver.Chrome(options=chrome_options)
